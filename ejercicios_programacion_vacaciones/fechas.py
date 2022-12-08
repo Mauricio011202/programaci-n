a1ae0da2547fb9ce5e1b0d1258c10e5db727cf5c
mauricio <m.mauricio@correo.unimet.edu.ve>
2022-12-08 09:40:53 -0400

    subir

--- ejercicios_programacion_vacaciones/fechas.py
+++ ejercicios_programacion_vacaciones/fechas.py
@@ -1,4 +1,4 @@
 mes= input('Por favor ingrese un mes')
-while not mes.isnumeric() and mes not in range(1,13):
+while not mes.isnumeric() and int(mes) not in range(1,13):
     mes= input('Por favor ingrese un mes')
-if 
+if mes 
