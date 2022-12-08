510c1b650af96e8ce02959203d1d802d732e80fd
mauricio <m.mauricio@correo.unimet.edu.ve>
2022-12-08 09:56:44 -0400

    subir

--- ejercicios_programacion_vacaciones/fechas.py
+++ ejercicios_programacion_vacaciones/fechas.py
@@ -1,18 +1,38 @@
+
+valido= True
 mes= input('Por favor ingrese un mes: ')
-while not mes.isnumeric() and int(mes) not in range(1,13):
+while not mes.isnumeric():
     mes= input('Por favor ingrese un mes: ')
+while  int(mes) not in range(1,13):
+    valido= False
+    break
 if mes in ['4','6','9','11']:
     dia= input('Ingrese el dia del mes: ')
-    while not dia.isnumeric() and int(dia) not in range(1,31):
+    while not dia.isnumeric() :
         dia= input('Ingrese el dia del mes valido: ')
+    while int(dia) not in range(1,31):
+        valido= False
+        break
 elif mes== '2':
     dia= input('Ingrese el dia del mes: ')
-    while not dia.isnumeric() and int(dia) not in range(1,29):
+    while not dia.isnumeric() :
         dia= input('Ingrese el dia del mes valido: ')
+    if  int(dia) not in range(1,29):
+        valido= False
 else:
     dia= input('Ingrese el dia del mes: ')
-    while not dia.isnumeric() and int(dia) not in range(1,32):
+    while not dia.isnumeric() :
         dia= input('Ingrese el dia del mes valido: ')
-        
+    if  int(dia) not in range(1,29):
+        valido= False
+anio= input('Ingrese el anio')
+if valido== True:
+    print('La fecha dia {} del mes {} del anio{} es valida'.format(dia,mes,anio))
+elif valido== False:
+    print('La fecha ingresada no es valida')
+
+
+
+
 
 
